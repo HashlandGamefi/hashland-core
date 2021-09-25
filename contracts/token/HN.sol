54298fa181e276962cc47bb768be605659e47400
havenquan <havenquan@qq.com>
2021-09-26 02:01:19 +0800

    hn

--- contracts/token/HN.sol
+++ contracts/token/HN.sol
@@ -19,11 +19,12 @@ contract HN is ERC721Enumerable, AccessControlEnumerable {
         uint256 level;
         uint256 race;
         uint256 class;
+        uint256[] hashrates;
         uint256[] attributes;
         uint256[] spells;
         uint256[] items;
         uint256[] metadatas;
-        uint256 spawnTime;
+        uint256 spawntime;
         uint256 seed;
     }
 
@@ -49,6 +50,7 @@ contract HN is ERC721Enumerable, AccessControlEnumerable {
         uint256 level,
         uint256 race,
         uint256 class,
+        uint256[] calldata hashrates,
         uint256[] calldata attributes,
         uint256[] calldata spells,
         uint256[] calldata items,
@@ -63,6 +65,7 @@ contract HN is ERC721Enumerable, AccessControlEnumerable {
                 level,
                 race,
                 class,
+                hashrates,
                 attributes,
                 spells,
                 items,
@@ -78,6 +81,7 @@ contract HN is ERC721Enumerable, AccessControlEnumerable {
                             level,
                             race,
                             class,
+                            hashrates,
                             attributes,
                             spells,
                             items,
@@ -95,9 +99,142 @@ contract HN is ERC721Enumerable, AccessControlEnumerable {
     }
 
     /**
-     * @dev Set Metadata
+     * @dev Set Ip
      */
-    function setMetadata(uint256 hnId, uint256[] calldata metadatas)
+    function setIp(uint256 hnId, uint256 ip) external onlyRole(SETTER_ROLE) {
+        Hn storage hn = hns[hnId];
+        hn.ip = ip;
+    }
+
+    /**
+     * @dev Set Level
+     */
+    function setLevel(uint256 hnId, uint256 level)
+        external
+        onlyRole(SETTER_ROLE)
+    {
+        Hn storage hn = hns[hnId];
+        hn.level = level;
+    }
+
+    /**
+     * @dev Set Race
+     */
+    function setRace(uint256 hnId, uint256 race)
+        external
+        onlyRole(SETTER_ROLE)
+    {
+        Hn storage hn = hns[hnId];
+        hn.race = race;
+    }
+
+    /**
+     * @dev Set Class
+     */
+    function setClass(uint256 hnId, uint256 class)
+        external
+        onlyRole(SETTER_ROLE)
+    {
+        Hn storage hn = hns[hnId];
+        hn.class = class;
+    }
+
+    /**
+     * @dev Set Hashrates
+     */
+    function setHashrates(uint256 hnId, uint256[] calldata hashrates)
+        external
+        onlyRole(SETTER_ROLE)
+    {
+        Hn storage hn = hns[hnId];
+        hn.hashrates = hashrates;
+    }
+
+    /**
+     * @dev Set Hashrate By Index
+     */
+    function setHashrateByIndex(
+        uint256 hnId,
+        uint256 hashrate,
+        uint256 index
+    ) external onlyRole(SETTER_ROLE) {
+        Hn storage hn = hns[hnId];
+        hn.hashrates[index] = hashrate;
+    }
+
+    /**
+     * @dev Set Attributes
+     */
+    function setAttributes(uint256 hnId, uint256[] calldata attributes)
+        external
+        onlyRole(SETTER_ROLE)
+    {
+        Hn storage hn = hns[hnId];
+        hn.attributes = attributes;
+    }
+
+    /**
+     * @dev Set Atrribute By Index
+     */
+    function setAtrributeByIndex(
+        uint256 hnId,
+        uint256 attribute,
+        uint256 index
+    ) external onlyRole(SETTER_ROLE) {
+        Hn storage hn = hns[hnId];
+        hn.attributes[index] = attribute;
+    }
+
+    /**
+     * @dev Set Spells
+     */
+    function setSpells(uint256 hnId, uint256[] calldata spells)
+        external
+        onlyRole(SETTER_ROLE)
+    {
+        Hn storage hn = hns[hnId];
+        hn.spells = spells;
+    }
+
+    /**
+     * @dev Set Spell By Index
+     */
+    function setSpellByIndex(
+        uint256 hnId,
+        uint256 spell,
+        uint256 index
+    ) external onlyRole(SETTER_ROLE) {
+        Hn storage hn = hns[hnId];
+        hn.spells[index] = spell;
+    }
+
+    /**
+     * @dev Set Items
+     */
+    function setItems(uint256 hnId, uint256[] calldata items)
+        external
+        onlyRole(SETTER_ROLE)
+    {
+        Hn storage hn = hns[hnId];
+        hn.items = items;
+    }
+
+    /**
+     * @dev Set Item By Index
+     */
+    function setItemByIndex(
+        uint256 hnId,
+        uint256 item,
+        uint256 index
+    ) external onlyRole(SETTER_ROLE) {
+        Hn storage hn = hns[hnId];
+        hn.items[index] = item;
+    }
+
+    /**
+     * @dev Set Metadatas
+     */
+    function setMetadatas(uint256 hnId, uint256[] calldata metadatas)
         external
         onlyRole(SETTER_ROLE)
     {
@@ -118,12 +255,155 @@ contract HN is ERC721Enumerable, AccessControlEnumerable {
     }
 
     /**
+     * @dev Set Seed
+     */
+    function setSeed(uint256 hnId, uint256 seed)
+        external
+        onlyRole(SETTER_ROLE)
+    {
+        Hn storage hn = hns[hnId];
+        hn.seed = seed;
+    }
+
+    /**
      * @dev Rename Hn
      */
     function renameHn(uint256 hnId, string calldata name) external {
         require(ownerOf(hnId) == msg.sender, "This Hn is not Own");
         Hn storage hn = hns[hnId];
         hn.name = name;
+    }
+
+    /**
+     * @dev Get Hn Name
+     */
+    function getHnName(uint256 hnId) external view returns (string memory) {
+        Hn memory hn = hns[hnId];
+        return hn.name;
+    }
+
+    /**
+     * @dev Get Hn Ip
+     */
+    function getHnIp(uint256 hnId) external view returns (uint256) {
+        Hn memory hn = hns[hnId];
+        return hn.ip;
+    }
+
+    /**
+     * @dev Get Hn Level
+     */
+    function getHnLevel(uint256 hnId) external view returns (uint256) {
+        Hn memory hn = hns[hnId];
+        return hn.level;
+    }
+
+    /**
+     * @dev Get Hn Race
+     */
+    function getHnRace(uint256 hnId) external view returns (uint256) {
+        Hn memory hn = hns[hnId];
+        return hn.race;
+    }
+
+    /**
+     * @dev Get Hn Class
+     */
+    function getHnClass(uint256 hnId) external view returns (uint256) {
+        Hn memory hn = hns[hnId];
+        return hn.class;
+    }
+
+    /**
+     * @dev Get Hn Hashrates
+     */
+    function getHnHashrates(uint256 hnId)
+        external
+        view
+        returns (uint256[] memory)
+    {
+        Hn memory hn = hns[hnId];
+        return hn.hashrates;
+    }
+
+    /**
+     * @dev Get Hn Hashrate By Index
+     */
+    function getHnHashrateByIndex(uint256 hnId, uint256 index)
+        external
+        view
+        returns (uint256)
+    {
+        Hn memory hn = hns[hnId];
+        return hn.hashrates[index];
+    }
+
+    /**
+     * @dev Get Hn Attributes
+     */
+    function getHnAttributes(uint256 hnId)
+        external
+        view
+        returns (uint256[] memory)
+    {
+        Hn memory hn = hns[hnId];
+        return hn.attributes;
+    }
+
+    /**
+     * @dev Get Hn Attribute By Index
+     */
+    function getHnAttributeByIndex(uint256 hnId, uint256 index)
+        external
+        view
+        returns (uint256)
+    {
+        Hn memory hn = hns[hnId];
+        return hn.attributes[index];
+    }
+
+    /**
+     * @dev Get Hn Spells
+     */
+    function getHnSpells(uint256 hnId)
+        external
+        view
+        returns (uint256[] memory)
+    {
+        Hn memory hn = hns[hnId];
+        return hn.spells;
+    }
+
+    /**
+     * @dev Get Hn Spell By Index
+     */
+    function getHnSpellByIndex(uint256 hnId, uint256 index)
+        external
+        view
+        returns (uint256)
+    {
+        Hn memory hn = hns[hnId];
+        return hn.spells[index];
+    }
+
+    /**
+     * @dev Get Hn Items
+     */
+    function getHnItems(uint256 hnId) external view returns (uint256[] memory) {
+        Hn memory hn = hns[hnId];
+        return hn.items;
+    }
+
+    /**
+     * @dev Get Hn Item By Index
+     */
+    function getHnItemByIndex(uint256 hnId, uint256 index)
+        external
+        view
+        returns (uint256)
+    {
+        Hn memory hn = hns[hnId];
+        return hn.items[index];
     }
 
     /**
@@ -151,11 +431,19 @@ contract HN is ERC721Enumerable, AccessControlEnumerable {
     }
 
     /**
-     * @dev Get Hn Race
+     * @dev Get Hn Spawntime
      */
-    function getHnRace(uint256 hnId) external view returns (uint256) {
+    function getHnSpawntime(uint256 hnId) external view returns (uint256) {
         Hn memory hn = hns[hnId];
-        return hn.race;
+        return hn.spawntime;
+    }
+
+    /**
+     * @dev Get Hn Seed
+     */
+    function getHnSeed(uint256 hnId) external view returns (uint256) {
+        Hn memory hn = hns[hnId];
+        return hn.seed;
     }
 
     /**
