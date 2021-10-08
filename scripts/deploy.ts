ad1dd842f02c6c013c3c9ad703dfb3970bd37dd6
havenquan <havenquan@qq.com>
2021-10-09 01:36:44 +0800

    upgrade

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,14 +2,12 @@ import hre, { ethers } from "hardhat";
 
 async function main() {
   const constructorArgs: any[] = [
-    "0xC71d48038A490DAF67BC0826b46E84C7e0467174",
-    "0x105A80A5Da83997c32818716846BB609C5Ffe35d",
     "0x105A80A5Da83997c32818716846BB609C5Ffe35d",
   ];
-  const factory = await ethers.getContractFactory("HNBox");
+  const factory = await ethers.getContractFactory("HC");
   const contract = await factory.deploy(...constructorArgs);
   await contract.deployed();
-  console.log("HNBox contract successfully deployed:", contract.address)
+  console.log("HC contract successfully deployed:", contract.address)
   await hre.run("verify:verify", {
     address: contract.address,
     constructorArguments: constructorArgs
