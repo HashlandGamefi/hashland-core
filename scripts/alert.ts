21014f59ccea08711165eda1f7da06f4b7238360
havenquan <havenquan@qq.com>
2021-10-28 19:47:29 +0800

    lp

--- scripts/alert.ts
+++ scripts/alert.ts
@@ -5,19 +5,23 @@ import TelegramBot from 'node-telegram-bot-api';
 async function main() {
   const token = process.env.TOKEN as string;
   const bot = new TelegramBot(token, { polling: true });
-  bot.onText(/\/echo (.+)/, (msg, match) => {
+
+  const hc = await ethers.getContractAt('HC', '0x20a3276972380E3c456137E49c32061498311Dd2');
+  const hclp = await ethers.getContractAt('HC', '0xdb83d062fa300fb8b00f6ceb79ecc71dfef921a5');
+  const filter = hc.filters.Transfer(null, '0xdb83d062fa300fb8b00f6ceb79ecc71dfef921a5')
+
+  bot.onText(/\/lp@HcAlertBot/, async (msg, match) => {
+    const totalSupply = await hclp.totalSupply();
     const chatId = msg.chat.id;
-    const message = (match as any)[1];
+    const message = `HC LP Total Supply: ${(totalSupply / 1e18).toFixed(4)}`;
     console.log(message);
     bot.sendMessage(chatId, message);
   });
 
-  const hc = await ethers.getContractAt('HC', '0x20a3276972380E3c456137E49c32061498311Dd2');
-  const filter = hc.filters.Transfer(null, '0xdb83d062fa300fb8b00f6ceb79ecc71dfef921a5')
   hc.on(filter, (from, to, amount, event) => {
     const message = `${from} sell ${utils.formatEther(amount)} HC`;
     console.log(message);
-    bot.sendMessage('@VivilyChan', message);
+    bot.sendMessage(-670292888, message);
   });
 }
 
