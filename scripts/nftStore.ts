377018c3f8ee3ccb6d358708c6f4ec78b8ea2f7a
havenquan <havenquan@qq.com>
2021-11-05 02:55:37 +0800

    nft

--- scripts/nftStore.ts
+++ scripts/nftStore.ts
@@ -4,6 +4,8 @@ import OSS from 'ali-oss';
 import { BigNumber, utils } from 'ethers';
 import sharp from 'sharp';
 
+const maxLevel = 5;
+
 // const client = new NFTStorage({ token: process.env.NFT_STORAGE_API_KEY as string });
 
 const client = new OSS({
@@ -20,37 +22,45 @@ function getRandomNumber(hnId: number, slot: string, base: number, range: number
 async function generateImage(hnId: number, level: number) {
     const hnClass = getRandomNumber(hnId, 'class', 1, 4);
 
-    const bg = sharp(`nft/materials/bg/${level}.png`).toBuffer();
+    const bg = sharp(`nft/bg/${level}.png`).toBuffer();
 
     const materials = [
-        `nft/materials/class${hnClass}/effect/bg/${level}.png`,
-        `nft/materials/class${hnClass}/hero.png`,
-        `nft/materials/class${hnClass}/item1/${getRandomNumber(hnId, 'item1', 1, 10)}.png`,
-        `nft/materials/class${hnClass}/item2/${getRandomNumber(hnId, 'item2', 1, 10)}.png`,
-        `nft/materials/class${hnClass}/effect/hero/${level}.png`,
-        `nft/materials/class${hnClass}/info.png`
+        `nft/class${hnClass}/effect/bg/${level}.png`,
+        `nft/class${hnClass}/hero.png`,
+        `nft/class${hnClass}/item1/${getRandomNumber(hnId, 'item1', 1, 10)}.png`,
+        `nft/class${hnClass}/item2/${getRandomNumber(hnId, 'item2', 1, 10)}.png`,
+        `nft/class${hnClass}/effect/hero/${level}.png`,
+        `nft/class${hnClass}/info.png`
     ].reduce(async (input, overlay) => {
         return await sharp(await input).composite([{ input: overlay }]).toBuffer();
     }, bg);
 
-    const composited = await sharp(await materials).sharpen().withMetadata().webp({ quality: 90 }).toBuffer();
+    const composited = await sharp(await materials).sharpen().webp({ quality: 90 }).toBuffer();
 
-    const result = await client.put(`nft/images/hashland-nft-${hnId}-${level}.png`, Buffer.from(composited));
+    const result = await client.put(`nft/images/hashland-nft-${hnId}-${level}.png`, composited);
     console.log(result.url);
 }
 
-function generateImages(start: number, end: number) {
-    for (let hnId = start; hnId < end; hnId++) {
-        for (let level = 1; level <= 5; level++) {
-            generateImage(hnId, level);
+async function generateImages(start: number, end: number) {
+    return new Promise(resolve => {
+        let count = 0;
+        for (let hnId = start; hnId < end; hnId++) {
+            for (let level = 1; level <= maxLevel; level++) {
+                generateImage(hnId, level).then(() => {
+                    count++;
+                    if (count == (end - start) * maxLevel) {
+                        resolve(true);
+                    }
+                });
+            }
         }
-    }
+    });
 }
 
 // async function uploadImages(start: number, end: number) {
 //     const images = [];
 //     for (let hnId = start; hnId < end; hnId++) {
-//         for (let level = 1; level <= 5; level++) {
+//         for (let level = 1; level <= maxLevel; level++) {
 //             const fileName = `hashland-nft-${hnId}-${level}.png`;
 //             const image = Buffer.from(fs.readFileSync(`nft/images/${fileName}`));
 //             images.push(new File([image], fileName, { type: 'image/webp' }));
@@ -100,7 +110,7 @@ async function generateMetadata(imagesCid: string, hnId: number, level: number) 
 
 function generateMetadatas(imagesCid: string, start: number, end: number) {
     for (let hnId = start; hnId < end; hnId++) {
-        for (let level = 1; level <= 5; level++) {
+        for (let level = 1; level <= maxLevel; level++) {
             generateMetadata(imagesCid, hnId, level);
         }
     }
@@ -109,7 +119,7 @@ function generateMetadatas(imagesCid: string, start: number, end: number) {
 // async function uploadMetadatas(start: number, end: number) {
 //     const metadatas = [];
 //     for (let hnId = start; hnId < end; hnId++) {
-//         for (let level = 1; level <= 5; level++) {
+//         for (let level = 1; level <= maxLevel; level++) {
 //             const fileName = `hashland-nft-${hnId}-${level}.json`;
 //             const json = fs.readFileSync(`nft/metadatas/${fileName}`).toString();
 //             metadatas.push(new File([json], fileName));
@@ -119,10 +129,13 @@ function generateMetadatas(imagesCid: string, start: number, end: number) {
 // }
 
 async function main() {
-    const start = 0;
-    const end = 10;
+    const totalSupply = 1000;
+    const batch = 100;
 
-    generateImages(start, end);
+    for (let i = 0; i < totalSupply / batch; i++) {
+        await generateImages(i * batch, (i + 1) * batch);
+        console.log(`${(i + 1) * batch} / ${totalSupply}`);
+    }
     // const newImageCID = await uploadImages(start, end);
     // console.log('New Image CID:', newImageCID);
 
