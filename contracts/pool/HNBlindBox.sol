c4eac02ddc153fd364d1ceb0a0ff049cd12b4d3a
havenquan <havenquan@qq.com>
2021-11-23 13:16:54 +0800

    tokeninfo

--- contracts/pool/HNBlindBox.sol
+++ contracts/pool/HNBlindBox.sol
@@ -21,15 +21,12 @@ contract HNBlindBox is AccessControlEnumerable, ReentrancyGuard {
 
     bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
 
-    address public receivingAddress;
-    uint256[] public boxTokenPrices = [100 * 1e18, 100 * 1e18];
-    address[] public tokenAddrs = [
-        0x0000000000000000000000000000000000000000,
-        0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56
-    ];
+    mapping(uint256 => uint256) public boxTokenPrices;
+    mapping(uint256 => address) public tokenAddrs;
+    mapping(uint256 => address) public receivingAddrs;
 
-    uint256 public boxesMaxSupply;
-    uint256 public totalBoxesLength;
+    mapping(uint256 => uint256) public boxesMaxSupply;
+    mapping(uint256 => uint256) public totalBoxesLength;
 
     uint256 public btcBase = 10000;
     uint256 public btcRange = 1000;
@@ -40,67 +37,54 @@ contract HNBlindBox is AccessControlEnumerable, ReentrancyGuard {
 
     EnumerableSet.AddressSet private users;
 
-    event SetTokensInfo(uint256[] boxTokenPrices, address[] tokenAddrs);
-    event SetReceivingAddress(address receivingAddr);
-    event AddBoxesMaxSupply(uint256 supply);
+    event SetTokenInfo(
+        uint256 tokenId,
+        uint256 boxTokenPrice,
+        address tokenAddr,
+        address receivingAddr
+    );
+    event AddBoxesMaxSupply(uint256 supply, uint256 tokenId);
     event SetDatas(uint256 btcBase, uint256 btcRange);
     event BuyBoxes(address indexed user, uint256 tokenId, uint256 price);
     event SpawnHns(address indexed user, uint256 boxesLength, uint256[] hnIds);
 
     /**
      * @param hnAddr Initialize HN Address
-     * @param receivingAddr Initialize Receiving Address
      * @param manager Initialize Manager Role
      */
-    constructor(
-        address hnAddr,
-        address receivingAddr,
-        address manager
-    ) {
+    constructor(address hnAddr, address manager) {
         hn = IHN(hnAddr);
-
-        receivingAddress = receivingAddr;
 
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _setupRole(MANAGER_ROLE, manager);
     }
 
     /**
-     * @dev Set Tokens Info
+     * @dev Set Token Info
      */
-    function setTokensInfo(
-        uint256[] calldata _boxTokenPrices,
-        address[] calldata _tokenAddrs
+    function setTokenInfo(
+        uint256 tokenId,
+        uint256 boxTokenPrice,
+        address tokenAddr,
+        address receivingAddr
     ) external onlyRole(MANAGER_ROLE) {
-        require(
-            _boxTokenPrices.length == _tokenAddrs.length,
-            "Tokens info length mismatch"
-        );
-        boxTokenPrices = _boxTokenPrices;
-        tokenAddrs = _tokenAddrs;
+        boxTokenPrices[tokenId] = boxTokenPrice;
+        tokenAddrs[tokenId] = tokenAddr;
+        receivingAddrs[tokenId] = receivingAddr;
 
-        emit SetTokensInfo(_boxTokenPrices, _tokenAddrs);
-    }
-
-    /**
-     * @dev Set Receiving Address
-     */
-    function setReceivingAddress(address receivingAddr)
-        external
-        onlyRole(MANAGER_ROLE)
-    {
-        receivingAddress = receivingAddr;
-
-        emit SetReceivingAddress(receivingAddr);
+        emit SetTokenInfo(tokenId, boxTokenPrice, tokenAddr, receivingAddr);
     }
 
     /**
      * @dev Add Boxes Max Supply
      */
-    function addBoxesMaxSupply(uint256 supply) external onlyRole(MANAGER_ROLE) {
-        boxesMaxSupply += supply;
+    function addBoxesMaxSupply(uint256 supply, uint256 tokenId)
+        external
+        onlyRole(MANAGER_ROLE)
+    {
+        boxesMaxSupply[tokenId] += supply;
 
-        emit AddBoxesMaxSupply(supply);
+        emit AddBoxesMaxSupply(supply, tokenId);
     }
 
     /**
@@ -126,27 +110,37 @@ contract HNBlindBox is AccessControlEnumerable, ReentrancyGuard {
     {
         require(boxesLength > 0, "Boxes length must > 0");
         require(boxesLength <= 256, "Boxes length must <= 256");
-        require(getBoxesLeftSupply() >= boxesLength, "Not enough boxes supply");
-        require(tokenId < tokenAddrs.length, "This token ID does not exist");
+        require(
+            getBoxesLeftSupply(tokenId) >= boxesLength,
+            "Not enough boxes supply"
+        );
         require(
             boxTokenPrices[tokenId] > 0,
             "The box price of this token has not been set"
+        );
+        require(
+            tokenAddrs[tokenId] != address(0),
+            "The token address of this token has not been set"
+        );
+        require(
+            receivingAddrs[tokenId] != address(0),
+            "The receiving address of this token has not been set"
         );
 
         uint256 price = boxesLength * boxTokenPrices[tokenId];
         if (tokenId == 0) {
             require(msg.value == price, "Price mismatch");
-            payable(receivingAddress).transfer(price);
+            payable(receivingAddrs[tokenId]).transfer(price);
         } else {
             IERC20 token = IERC20(tokenAddrs[tokenId]);
-            token.safeTransferFrom(msg.sender, receivingAddress, price);
+            token.safeTransferFrom(msg.sender, receivingAddrs[tokenId], price);
         }
 
-        spawnHns(msg.sender, boxesLength);
+        spawnHns(msg.sender, boxesLength, tokenId);
 
         userBoxesLength[msg.sender] += boxesLength;
         userTokenBuyAmount[msg.sender][tokenId] += price;
-        totalBoxesLength += boxesLength;
+        totalBoxesLength[tokenId] += boxesLength;
         totalTokenBuyAmount[tokenId] += price;
         users.add(msg.sender);
 
@@ -154,14 +148,22 @@ contract HNBlindBox is AccessControlEnumerable, ReentrancyGuard {
     }
 
     /**
-     * @dev Get Tokens Info
+     * @dev Get Token Info
      */
-    function getTokensInfo()
+    function getTokenInfo(uint256 tokenId)
         external
         view
-        returns (uint256[] memory _boxTokenPrices, address[] memory _tokenAddrs)
+        returns (
+            uint256,
+            address,
+            address
+        )
     {
-        return (boxTokenPrices, tokenAddrs);
+        return (
+            boxTokenPrices[tokenId],
+            tokenAddrs[tokenId],
+            receivingAddrs[tokenId]
+        );
     }
 
     /**
@@ -195,14 +197,18 @@ contract HNBlindBox is AccessControlEnumerable, ReentrancyGuard {
     /**
      * @dev Get Boxes Left Supply
      */
-    function getBoxesLeftSupply() public view returns (uint256) {
-        return boxesMaxSupply - totalBoxesLength;
+    function getBoxesLeftSupply(uint256 tokenId) public view returns (uint256) {
+        return boxesMaxSupply[tokenId] - totalBoxesLength[tokenId];
     }
 
     /**
      * @dev Spawn Hns
      */
-    function spawnHns(address to, uint256 boxesLength) private {
+    function spawnHns(
+        address to,
+        uint256 boxesLength,
+        uint256 tokenId
+    ) private {
         uint256[] memory hnIds = new uint256[](boxesLength);
         for (uint256 i = 0; i < boxesLength; i++) {
             uint256 randomness = uint256(
@@ -211,9 +217,9 @@ contract HNBlindBox is AccessControlEnumerable, ReentrancyGuard {
                         to,
                         block.number,
                         boxesLength,
-                        boxTokenPrices,
-                        boxesMaxSupply,
-                        totalBoxesLength,
+                        boxTokenPrices[tokenId],
+                        boxesMaxSupply[tokenId],
+                        totalBoxesLength[tokenId],
                         userBoxesLength[to],
                         users.length(),
                         i
