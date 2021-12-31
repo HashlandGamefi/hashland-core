cfcc0271d857a52c3bc13eb354c3431e7724e41c
havenquan <havenquan@qq.com>
2021-12-31 20:32:15 +0800

    nfts2

---
+++ contracts/pool/HNBlindBoxS2.sol
@@ -0,0 +1,555 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+import "../token/interface/IHN.sol";
+
+/**
+ * @title HN-S2 Blind Box Contract
+ * @author HASHLAND-TEAM
+ * @notice In this contract users can draw high level HN-S2
+ */
+contract HNBlindBoxS2 is
+    AccessControlEnumerable,
+    VRFConsumerBase,
+    ReentrancyGuard
+{
+    using SafeERC20 for IERC20;
+    using EnumerableSet for EnumerableSet.AddressSet;
+    using EnumerableSet for EnumerableSet.UintSet;
+
+    IHN public hn;
+
+    bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
+
+    mapping(uint256 => uint256) public boxTokenPrices;
+    mapping(uint256 => address) public tokenAddrs;
+    mapping(uint256 => address) public receivingAddrs;
+    mapping(uint256 => uint256) public hourlyBuyLimits;
+    mapping(uint256 => bool) public whiteListFlags;
+    mapping(uint256 => bool) public vrfFlags;
+    mapping(uint256 => uint256[]) public levelProbabilities;
+
+    mapping(uint256 => uint256) public boxesMaxSupply;
+    mapping(uint256 => uint256) public totalBoxesLength;
+
+    uint256 public epicRate = 100;
+    uint256[] public hashrateBases = [10000, 44000, 211200, 1098240, 6150144];
+    uint256[] public hashrateRanges = [1000, 8800, 63360, 439296, 3075072];
+
+    mapping(uint256 => uint256) public totalTokenBuyAmount;
+    mapping(address => uint256) public userBoxesLength;
+    mapping(address => mapping(uint256 => uint256)) public userTokenBuyAmount;
+    mapping(address => mapping(uint256 => uint256))
+        public userHourlyBoxesLength;
+
+    EnumerableSet.AddressSet private users;
+    mapping(uint256 => EnumerableSet.UintSet) private levelHnIds;
+    mapping(uint256 => EnumerableSet.AddressSet) private whiteList;
+
+    mapping(bytes32 => address) public requestIdToUser;
+    mapping(bytes32 => uint256) public requestIdToBoxesLength;
+    mapping(bytes32 => uint256) public requestIdToTokenId;
+
+    bytes32 public keyHash;
+    uint256 public fee;
+
+    event SetTokenInfo(
+        uint256 tokenId,
+        uint256 boxTokenPrice,
+        address tokenAddr,
+        address receivingAddr,
+        uint256 hourlyBuylimit,
+        bool whiteListFlag,
+        bool vrfFlag,
+        uint256[] levelProbability
+    );
+    event AddBoxesMaxSupply(uint256 supply, uint256 tokenId);
+    event AddWhiteList(uint256 tokenId, address[] whiteUsers);
+    event RemoveWhiteList(uint256 tokenId, address[] whiteUsers);
+    event BuyBoxes(address indexed user, uint256 tokenId, uint256 price);
+    event SpawnHns(
+        address indexed user,
+        uint256 boxesLength,
+        uint256[] hnIds,
+        uint256[] levels
+    );
+
+    /**
+     * @param vrfAddr Initialize VRF Coordinator Address
+     * @param linkAddr Initialize LINK Token Address
+     * @param _keyHash Initialize Key Hash
+     * @param _fee Initialize Fee
+     * @param hnAddr Initialize HN Address
+     * @param manager Initialize Manager Role
+     */
+    constructor(
+        address vrfAddr,
+        address linkAddr,
+        bytes32 _keyHash,
+        uint256 _fee,
+        address hnAddr,
+        address manager
+    ) VRFConsumerBase(vrfAddr, linkAddr) {
+        keyHash = _keyHash;
+        fee = _fee;
+
+        hn = IHN(hnAddr);
+
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(MANAGER_ROLE, manager);
+    }
+
+    /**
+     * @dev Set Key Hash
+     */
+    function setKeyHash(bytes32 _keyHash) external onlyRole(MANAGER_ROLE) {
+        keyHash = _keyHash;
+    }
+
+    /**
+     * @dev Set Fee
+     */
+    function setFee(uint256 _fee) external onlyRole(MANAGER_ROLE) {
+        fee = _fee;
+    }
+
+    /**
+     * @dev Set Token Info
+     */
+    function setTokenInfo(
+        uint256 tokenId,
+        uint256 boxTokenPrice,
+        address tokenAddr,
+        address receivingAddr,
+        uint256 hourlyBuyLimit,
+        bool whiteListFlag,
+        bool vrfFlag,
+        uint256[] calldata levelProbability
+    ) external onlyRole(MANAGER_ROLE) {
+        boxTokenPrices[tokenId] = boxTokenPrice;
+        tokenAddrs[tokenId] = tokenAddr;
+        receivingAddrs[tokenId] = receivingAddr;
+        hourlyBuyLimits[tokenId] = hourlyBuyLimit;
+        whiteListFlags[tokenId] = whiteListFlag;
+        vrfFlags[tokenId] = vrfFlag;
+        levelProbabilities[tokenId] = levelProbability;
+
+        emit SetTokenInfo(
+            tokenId,
+            boxTokenPrice,
+            tokenAddr,
+            receivingAddr,
+            hourlyBuyLimit,
+            whiteListFlag,
+            vrfFlag,
+            levelProbability
+        );
+    }
+
+    /**
+     * @dev Add Boxes Max Supply
+     */
+    function addBoxesMaxSupply(uint256 supply, uint256 tokenId)
+        external
+        onlyRole(MANAGER_ROLE)
+    {
+        if (vrfFlags[tokenId]) {
+            LINK.transferFrom(msg.sender, address(this), supply * fee);
+        }
+        boxesMaxSupply[tokenId] += supply;
+
+        emit AddBoxesMaxSupply(supply, tokenId);
+    }
+
+    /**
+     * @dev Add White List
+     */
+    function addWhiteList(uint256 tokenId, address[] calldata whiteUsers)
+        external
+        onlyRole(MANAGER_ROLE)
+    {
+        for (uint256 i = 0; i < whiteUsers.length; i++) {
+            whiteList[tokenId].add(whiteUsers[i]);
+        }
+
+        emit AddWhiteList(tokenId, whiteUsers);
+    }
+
+    /**
+     * @dev Remove White List
+     */
+    function removeWhiteList(uint256 tokenId, address[] calldata whiteUsers)
+        external
+        onlyRole(MANAGER_ROLE)
+    {
+        for (uint256 i = 0; i < whiteUsers.length; i++) {
+            whiteList[tokenId].remove(whiteUsers[i]);
+        }
+
+        emit RemoveWhiteList(tokenId, whiteUsers);
+    }
+
+    /**
+     * @dev Buy Boxes
+     */
+    function buyBoxes(uint256 boxesLength, uint256 tokenId)
+        external
+        payable
+        nonReentrant
+    {
+        require(boxesLength > 0, "Boxes length must > 0");
+        require(
+            getUserHourlyBoxesLeftSupply(
+                tokenId,
+                msg.sender,
+                block.timestamp
+            ) >= boxesLength,
+            "Boxes length exceeds the hourly buy limit"
+        );
+        require(
+            getBoxesLeftSupply(tokenId) >= boxesLength,
+            "Not enough boxes supply"
+        );
+        require(
+            boxTokenPrices[tokenId] > 0,
+            "The box price of this token has not been set"
+        );
+        require(
+            tokenAddrs[tokenId] != address(0),
+            "The token address of this token has not been set"
+        );
+        require(
+            receivingAddrs[tokenId] != address(0),
+            "The receiving address of this token has not been set"
+        );
+        require(
+            levelProbabilities[tokenId].length == 5,
+            "The level probability of this token has not been set"
+        );
+        if (whiteListFlags[tokenId]) {
+            require(
+                whiteList[tokenId].contains(msg.sender),
+                "Your address must be on the whitelist"
+            );
+        }
+
+        uint256 price = boxesLength * boxTokenPrices[tokenId];
+        if (tokenId == 0) {
+            require(msg.value == price, "Price mismatch");
+            payable(receivingAddrs[tokenId]).transfer(price);
+        } else {
+            IERC20 token = IERC20(tokenAddrs[tokenId]);
+            token.safeTransferFrom(msg.sender, receivingAddrs[tokenId], price);
+        }
+
+        if (vrfFlags[tokenId]) {
+            require(LINK.balanceOf(address(this)) >= fee, "Not Enough LINK");
+            bytes32 requestId = requestRandomness(keyHash, fee);
+            requestIdToUser[requestId] = msg.sender;
+            requestIdToBoxesLength[requestId] = boxesLength;
+            requestIdToTokenId[requestId] = tokenId;
+        } else {
+            spawnHns(msg.sender, boxesLength, tokenId);
+        }
+
+        userBoxesLength[msg.sender] += boxesLength;
+        userHourlyBoxesLength[msg.sender][
+            block.timestamp / 3600
+        ] += boxesLength;
+        userTokenBuyAmount[msg.sender][tokenId] += price;
+        totalBoxesLength[tokenId] += boxesLength;
+        totalTokenBuyAmount[tokenId] += price;
+        users.add(msg.sender);
+
+        emit BuyBoxes(msg.sender, tokenId, price);
+    }
+
+    /**
+     * @dev Get Token Info
+     */
+    function getTokenInfo(uint256 tokenId)
+        external
+        view
+        returns (
+            uint256,
+            address,
+            address,
+            uint256,
+            bool,
+            bool,
+            uint256[] memory
+        )
+    {
+        return (
+            boxTokenPrices[tokenId],
+            tokenAddrs[tokenId],
+            receivingAddrs[tokenId],
+            hourlyBuyLimits[tokenId],
+            whiteListFlags[tokenId],
+            vrfFlags[tokenId],
+            levelProbabilities[tokenId]
+        );
+    }
+
+    /**
+     * @dev Get Users Length
+     */
+    function getUsersLength() external view returns (uint256) {
+        return users.length();
+    }
+
+    /**
+     * @dev Get Users by Size
+     */
+    function getUsersBySize(uint256 cursor, uint256 size)
+        external
+        view
+        returns (address[] memory, uint256)
+    {
+        uint256 length = size;
+        if (length > users.length() - cursor) {
+            length = users.length() - cursor;
+        }
+
+        address[] memory values = new address[](length);
+        for (uint256 i = 0; i < length; i++) {
+            values[i] = users.at(cursor + i);
+        }
+
+        return (values, cursor + length);
+    }
+
+    /**
+     * @dev Get Each Level HnIds Length
+     */
+    function getEachLevelHnIdsLength(uint256 maxLevel)
+        external
+        view
+        returns (uint256[] memory)
+    {
+        uint256[] memory lengths = new uint256[](maxLevel);
+        for (uint256 i = 0; i < maxLevel; i++) {
+            lengths[i] = levelHnIds[i + 1].length();
+        }
+        return lengths;
+    }
+
+    /**
+     * @dev Get Level HnIds Length
+     */
+    function getLevelHnIdsLength(uint256 level)
+        external
+        view
+        returns (uint256)
+    {
+        return levelHnIds[level].length();
+    }
+
+    /**
+     * @dev Get Level HnIds by Size
+     */
+    function getLevelHnIdsBySize(
+        uint256 level,
+        uint256 cursor,
+        uint256 size
+    ) external view returns (uint256[] memory, uint256) {
+        uint256 length = size;
+        if (length > levelHnIds[level].length() - cursor) {
+            length = levelHnIds[level].length() - cursor;
+        }
+
+        uint256[] memory values = new uint256[](length);
+        for (uint256 i = 0; i < length; i++) {
+            values[i] = levelHnIds[level].at(cursor + i);
+        }
+
+        return (values, cursor + length);
+    }
+
+    /**
+     * @dev Get White List Existence
+     */
+    function getWhiteListExistence(uint256 tokenId, address user)
+        external
+        view
+        returns (bool)
+    {
+        return whiteList[tokenId].contains(user);
+    }
+
+    /**
+     * @dev Get White List Length
+     */
+    function getWhiteListLength(uint256 tokenId)
+        external
+        view
+        returns (uint256)
+    {
+        return whiteList[tokenId].length();
+    }
+
+    /**
+     * @dev Get White List by Size
+     */
+    function getWhiteListBySize(
+        uint256 tokenId,
+        uint256 cursor,
+        uint256 size
+    ) external view returns (address[] memory, uint256) {
+        uint256 length = size;
+        if (length > whiteList[tokenId].length() - cursor) {
+            length = whiteList[tokenId].length() - cursor;
+        }
+
+        address[] memory values = new address[](length);
+        for (uint256 i = 0; i < length; i++) {
+            values[i] = whiteList[tokenId].at(cursor + i);
+        }
+
+        return (values, cursor + length);
+    }
+
+    /**
+     * @dev Get Boxes Left Supply
+     */
+    function getBoxesLeftSupply(uint256 tokenId) public view returns (uint256) {
+        return boxesMaxSupply[tokenId] - totalBoxesLength[tokenId];
+    }
+
+    /**
+     * @dev Get User Hourly Boxes Left Supply
+     */
+    function getUserHourlyBoxesLeftSupply(
+        uint256 tokenId,
+        address user,
+        uint256 timestamp
+    ) public view returns (uint256) {
+        return
+            hourlyBuyLimits[tokenId] -
+            userHourlyBoxesLength[user][timestamp / 3600];
+    }
+
+    /**
+     * @dev Get Level
+     */
+    function getLevel(uint256 tokenId, uint256 random)
+        public
+        view
+        returns (uint256)
+    {
+        uint256 accProbability;
+        uint256 level;
+        for (uint256 i = 0; i < levelProbabilities[tokenId].length; i++) {
+            accProbability += levelProbabilities[tokenId][i];
+            if (random < accProbability) {
+                level = i;
+                break;
+            }
+        }
+        return level + 1;
+    }
+
+    /**
+     * @dev Spawn HN to User when get Randomness Response
+     */
+    function fulfillRandomness(bytes32 requestId, uint256 randomness)
+        internal
+        override
+    {
+        uint256[] memory hnIds = new uint256[](
+            requestIdToBoxesLength[requestId]
+        );
+        uint256[] memory levels = new uint256[](
+            requestIdToBoxesLength[requestId]
+        );
+        for (uint256 i = 0; i < requestIdToBoxesLength[requestId]; i++) {
+            uint256 level = getLevel(
+                requestIdToTokenId[requestId],
+                randomness % 1e4
+            );
+            uint256 hcBase = level >= 2 ? hashrateBases[level - 2] : 0;
+            uint256 hcRange = level >= 2 ? hashrateRanges[level - 2] : 1;
+
+            uint256[] memory hashrates = new uint256[](2);
+            hashrates[0] = hcBase + (((randomness % 1e14) / 1e4) % hcRange);
+
+            uint256 hnId = hn.spawnHn(
+                requestIdToUser[requestId],
+                1,
+                2,
+                level,
+                hashrates
+            );
+
+            if (((randomness % 1e18) / 1e14) < epicRate) {
+                hn.setData(hnId, "epic", 1);
+            }
+
+            hnIds[i] = hnId;
+            levels[i] = level;
+            levelHnIds[level].add(hnId);
+            randomness /= 1e18;
+        }
+
+        emit SpawnHns(
+            requestIdToUser[requestId],
+            requestIdToBoxesLength[requestId],
+            hnIds,
+            levels
+        );
+    }
+
+    /**
+     * @dev Spawn Hns
+     */
+    function spawnHns(
+        address to,
+        uint256 boxesLength,
+        uint256 tokenId
+    ) private {
+        uint256[] memory hnIds = new uint256[](boxesLength);
+        uint256[] memory levels = new uint256[](boxesLength);
+        for (uint256 i = 0; i < boxesLength; i++) {
+            uint256 randomness = uint256(
+                keccak256(
+                    abi.encodePacked(
+                        to,
+                        block.number,
+                        boxesLength,
+                        boxTokenPrices[tokenId],
+                        boxesMaxSupply[tokenId],
+                        totalBoxesLength[tokenId],
+                        userBoxesLength[to],
+                        users.length(),
+                        i
+                    )
+                )
+            );
+
+            uint256 level = getLevel(tokenId, randomness % 1e4);
+            uint256 hcBase = level >= 2 ? hashrateBases[level - 2] : 0;
+            uint256 hcRange = level >= 2 ? hashrateRanges[level - 2] : 1;
+
+            uint256[] memory hashrates = new uint256[](2);
+            hashrates[0] = hcBase + (((randomness % 1e14) / 1e4) % hcRange);
+
+            uint256 hnId = hn.spawnHn(to, 1, 2, level, hashrates);
+
+            if (((randomness % 1e18) / 1e14) < epicRate) {
+                hn.setData(hnId, "epic", 1);
+            }
+
+            hnIds[i] = hnId;
+            levels[i] = level;
+            levelHnIds[level].add(hnId);
+        }
+
+        emit SpawnHns(to, boxesLength, hnIds, levels);
+    }
+}
---
+++ scripts/nftStoreS2.ts
@@ -0,0 +1,223 @@
+import OSS from 'ali-oss';
+import { BigNumber, utils } from 'ethers';
+import { ethers } from 'hardhat';
+import { resolve } from 'path/posix';
+import sharp from 'sharp';
+
+const maxLevel = 5;
+const hnAddr = '0xEEa8bD31DA9A2169C38968958B6DF216381B0f08'
+
+const client = new OSS({
+    region: 'oss-ap-southeast-1',
+    accessKeyId: process.env.accessKeyId as string,
+    accessKeySecret: process.env.accessKeySecret as string,
+    bucket: 'hashlandgamefi',
+});
+
+function getRandomNumber(hnId: number, slot: string, base: number, range: number) {
+    return BigNumber.from(utils.solidityKeccak256(['uint256', 'string'], [hnId, slot])).mod(range).add(base).toNumber();
+}
+
+async function main() {
+    const hn = await ethers.getContractAt('HN', hnAddr);
+
+    async function generateImageByLevel(hnId: number, level: number) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const series = 2;
+                const hnClass = getRandomNumber(hnId, 'class', 1, 4);
+
+                const heroItems = [
+                    `nft/s${series}/class${hnClass}/hero.png`,
+                    `nft/s${series}/class${hnClass}/item1/${getRandomNumber(hnId, 'item1', 1, 10)}.png`,
+                    `nft/s${series}/class${hnClass}/item2/${getRandomNumber(hnId, 'item2', 1, 10)}.png`,
+                    `nft/s${series}/class${hnClass}/item3/${getRandomNumber(hnId, 'item3', 1, 10)}.png`,
+                    `nft/s${series}/class${hnClass}/item4/${getRandomNumber(hnId, 'item4', 1, 10)}.png`,
+                    `nft/s${series}/class${hnClass}/item5/${getRandomNumber(hnId, 'item5', 1, 10)}.png`,
+                    `nft/s${series}/class${hnClass}/item6/${getRandomNumber(hnId, 'item6', 1, 10)}.png`,
+                    `nft/s${series}/class${hnClass}/item7/${getRandomNumber(hnId, 'item7', 1, 10)}.png`,
+                    `nft/s${series}/class${hnClass}/item8/${getRandomNumber(hnId, 'item8', 1, 10)}.png`,
+                ];
+
+                const heroItemsByLevel = [
+                    [],
+                    [heroItems[0], heroItems[1], heroItems[2]],
+                    [heroItems[0], heroItems[1], heroItems[3], heroItems[4]],
+                    [heroItems[0], heroItems[1], heroItems[3], heroItems[5], heroItems[6]],
+                    hnClass == 4 ? [heroItems[0], heroItems[1], heroItems[3], heroItems[5], heroItems[6], heroItems[7]] : [heroItems[0], heroItems[1], heroItems[7], heroItems[3], heroItems[5], heroItems[6]],
+                    hnClass == 4 ? [heroItems[0], heroItems[1], heroItems[3], heroItems[5], heroItems[6], heroItems[7], heroItems[8]] : [heroItems[0], heroItems[1], heroItems[7], heroItems[3], heroItems[5], heroItems[6], heroItems[8]],
+                ];
+
+                const bg = sharp(`nft/s${series}/bg/${level}.png`).toBuffer();
+                const materials = [
+                    `nft/s${series}/class${hnClass}/effect/bg/${level}.png`,
+                    ...heroItemsByLevel[level],
+                    `nft/s${series}/class${hnClass}/effect/hero/${level}.png`,
+                    `nft/s${series}/class${hnClass}/info.png`,
+                    `nft/s${series}/bar/${level}.png`,
+                ].reduce(async (input, overlay) => {
+                    return await sharp(await input).composite([{ input: overlay }]).toBuffer();
+                }, bg);
+
+                const composited = await sharp(await materials).sharpen().webp({ quality: 90 }).toBuffer();
+
+                await client.put(`nft/images/hashland-nft-${hnId}-${level}.png`, composited);
+                resolve(true);
+            } catch (e) {
+                reject(e);
+            }
+        });
+    }
+
+    async function generateAllLevelImages(hnId: number) {
+        return new Promise((resolve, reject) => {
+            let count = 0;
+            for (let level = 1; level <= maxLevel; level++) {
+                generateImageByLevel(hnId, level).then(() => {
+                    count++;
+                    if (count == maxLevel) {
+                        resolve(true);
+                    }
+                }).catch(e => {
+                    reject(e);
+                });
+            }
+        });
+    }
+
+    async function generateMetadataByLevel(imagesCid: string, hnId: number, level: number) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const hnClass = getRandomNumber(hnId, 'class', 1, 4);
+                const hashrates = await hn.getHashrates(hnId);
+                const className = ['Cavalryman', 'Holy', 'Blade', 'Hex'];
+                const heroName = ['Tameka', 'Katniss', 'Natalie', `Mila`];
+                const fileName = `hashland-nft-${hnId}-${level}`;
+
+                const watermark = `watermark,text_${Buffer.from((hashrates[0] / 1e4).toFixed(4)).toString('base64url')},type_enpnZnhpbmd5YW4,color_ffffff,size_24,g_nw,x_395,y_79/watermark,text_${Buffer.from((hashrates[1] / 1e4).toFixed(4)).toString('base64url')},type_enpnZnhpbmd5YW4,color_ffffff,size_24,g_nw,x_655,y_79`;
+
+                const metadata = {
+                    name: `HashLand NFT #${hnId}`,
+                    description: 'Have you ever imagined an NFT with BTC hashrate? HashLand did it, and now he brings the first series of NFT - I AM MT.',
+                    image: `${imagesCid}/${fileName}.png?image_process=${watermark}`,
+                    attributes: [
+                        {
+                            trait_type: 'Ip',
+                            value: `Hash Warfare`,
+                        },
+                        {
+                            trait_type: 'Series',
+                            value: 'Series 2',
+                        },
+                        {
+                            trait_type: 'Level',
+                            value: level,
+                        },
+                        {
+                            trait_type: 'Class',
+                            value: className[hnClass - 1],
+                        },
+                        {
+                            trait_type: 'Hero',
+                            value: heroName[hnClass - 1],
+                        },
+                        {
+                            trait_type: 'HC_Hashrate',
+                            value: (hashrates[0] / 1e4).toFixed(4),
+                        },
+                    ],
+                }
+
+                await client.put(`nft/metadatas/${fileName}.json`, Buffer.from(JSON.stringify(metadata)));
+                resolve(true);
+            } catch (e) {
+                reject(e);
+            }
+        });
+    }
+
+    async function generateAllLevelMetadatas(imagesCid: string, hnId: number) {
+        return new Promise((resolve, reject) => {
+            let count = 0;
+            for (let level = 1; level <= maxLevel; level++) {
+                generateMetadataByLevel(imagesCid, hnId, level).then(() => {
+                    count++;
+                    if (count == maxLevel) {
+                        resolve(true);
+                    }
+                }).catch((e) => {
+                    reject(e);
+                });
+            }
+        });
+    }
+
+    async function generateAllLevelImagesAndMetadatas(imagesCid: string, hnId: number) {
+        return new Promise((resolve, reject) => {
+            generateAllLevelImages(hnId).then(() => {
+                generateAllLevelMetadatas(imagesCid, hnId).then(() => {
+                    resolve(true);
+                }).catch((e) => {
+                    reject(e);
+                });
+            }).catch((e) => {
+                reject(e);
+            });
+        });
+    }
+
+    function updateMetadata() {
+        hn.on('SpawnHn', async (to, hnId, event) => {
+            generateAllLevelMetadatas('https://cdn.hashland.com/nft/images', hnId);
+            console.log(`Spawn NFT #${hnId} to ${to}`);
+        });
+
+        hn.on('SetHashrates', async (hnId, hashrates, event) => {
+            generateAllLevelMetadatas('https://cdn.hashland.com/nft/images', hnId);
+            console.log(`Set NFT #${hnId} hashrates to [${(hashrates[0] / 1e4).toFixed(4)}, ${(hashrates[1] / 1e4).toFixed(4)}]`);
+        });
+
+        const filter = hn.filters.Transfer(null, '0xe0A9e5B59701a776575fDd6257c3F89Ae362629a');
+        hn.on(filter, async (from, to, hnId, event) => {
+            generateAllLevelMetadatas('https://cdn.hashland.com/nft/images', hnId);
+            console.log(`Transfer NFT #${hnId} from ${from} to Binance NFT Market`);
+        });
+    }
+
+    const start = 60010;
+    const end = 60100;
+    const batch = 10;
+    const set: Set<number> = new Set();
+    for (let i = start; i < end; i++) {
+        set.add(i);
+    }
+
+    async function uploadAllImagesAndMetadatas() {
+        return new Promise(resolve => {
+            const iterator = set.values();
+            let count = 0;
+            for (let i = 0; i < batch; i++) {
+                const hnId = iterator.next();
+                if (hnId.done) {
+                    break;
+                }
+                generateAllLevelImagesAndMetadatas('https://cdn.hashland.com/nft/images', hnId.value).then(() => {
+                    set.delete(hnId.value);
+                    console.log(`(${end - start - set.size}/${end - start}) NFT #${hnId.value} datas uploaded successfully`);
+                }).catch(e => {
+                    console.log(`(${end - start - set.size}/${end - start}) NFT #${hnId.value} datas uploaded failed`);
+                }).finally(async () => {
+                    count++;
+                    if (count == batch) {
+                        resolve(await uploadAllImagesAndMetadatas());
+                    }
+                });
+            }
+        });
+    }
+
+    // updateMetadata();
+    await uploadAllImagesAndMetadatas();
+}
+
+main();
